74025bf7056e8a84c4e93190d920caa7b2fb9344
joshua <joshares9991@gmail.com>
2024-01-15 08:02:40 +0000

    remove auto zoom for mobile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,20 @@
 import Providers from "@/components/Providers";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <Providers>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="theme-color" content="" />
+        <meta name="description" content="Ricky and morty characters website" />
+        <link rel="icon" href="/ricky.png" />
+      </Head>
       <Component {...pageProps} />
     </Providers>
   );
